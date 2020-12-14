83e7cac6393218cff987eef0d653a95abd502109
JulenBarro <71315113+JulenBarro@users.noreply.github.com>
2020-12-14 13:31:17 +0100

    Update HibernateDataAccess.java

--- BetsJSF2-Hibernate/src/dataAccess/HibernateDataAccess.java
+++ BetsJSF2-Hibernate/src/dataAccess/HibernateDataAccess.java
@@ -11,7 +11,17 @@ import domain.Question;
 import modelo.HibernateUtil;
 
 public class HibernateDataAccess {
+	public HibernateDataAccess(boolean initializeMode) {
+		if(initializeMode) {
+			initializeDB();
+		}
+	}
+	
 	public HibernateDataAccess() {
+		new HibernateDataAccess(false);
+	}
+	
+	public void initializeDB(){
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
@@ -22,27 +32,27 @@ public class HibernateDataAccess {
 		int year=today.get(Calendar.YEAR);
 		if (month==12) { month=0; year+=1;}  
 
-		Event ev1=new Event(1, "Atlético-Athletic", UtilDate.newDate(year,month,17));
+		Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(year,month,17));
 		Event ev2=new Event(2, "Eibar-Barcelona", UtilDate.newDate(year,month,17));
 		Event ev3=new Event(3, "Getafe-Celta", UtilDate.newDate(year,month,17));
-		Event ev4=new Event(4, "Alavés-Deportivo", UtilDate.newDate(year,month,17));
-		Event ev5=new Event(5, "Español-Villareal", UtilDate.newDate(year,month,17));
+		Event ev4=new Event(4, "Alaves-Deportivo", UtilDate.newDate(year,month,17));
+		Event ev5=new Event(5, "Espanol-Villareal", UtilDate.newDate(year,month,17));
 		Event ev6=new Event(6, "Las Palmas-Sevilla", UtilDate.newDate(year,month,17));
 		Event ev7=new Event(7, "Malaga-Valencia", UtilDate.newDate(year,month,17));
-		Event ev8=new Event(8, "Girona-Leganés", UtilDate.newDate(year,month,17));
+		Event ev8=new Event(8, "Girona-Leganes", UtilDate.newDate(year,month,17));
 		Event ev9=new Event(9, "Real Sociedad-Levante", UtilDate.newDate(year,month,17));
 		Event ev10=new Event(10, "Betis-Real Madrid", UtilDate.newDate(year,month,17));
 
 		Event ev11=new Event(11, "Atletico-Athletic", UtilDate.newDate(year,month,1));
 		Event ev12=new Event(12, "Eibar-Barcelona", UtilDate.newDate(year,month,1));
 		Event ev13=new Event(13, "Getafe-Celta", UtilDate.newDate(year,month,1));
-		Event ev14=new Event(14, "Alavés-Deportivo", UtilDate.newDate(year,month,1));
-		Event ev15=new Event(15, "Español-Villareal", UtilDate.newDate(year,month,1));
+		Event ev14=new Event(14, "Alaves-Deportivo", UtilDate.newDate(year,month,1));
+		Event ev15=new Event(15, "Espanol-Villareal", UtilDate.newDate(year,month,1));
 		Event ev16=new Event(16, "Las Palmas-Sevilla", UtilDate.newDate(year,month,1));
 
 
-		Event ev17=new Event(17, "Málaga-Valencia", UtilDate.newDate(year,month,28));
-		Event ev18=new Event(18, "Girona-Leganés", UtilDate.newDate(year,month,28));
+		Event ev17=new Event(17, "Malaga-Valencia", UtilDate.newDate(year,month,28));
+		Event ev18=new Event(18, "Girona-Leganes", UtilDate.newDate(year,month,28));
 		Event ev19=new Event(19, "Real Sociedad-Levante", UtilDate.newDate(year,month,28));
 		Event ev20=new Event(20, "Betis-Real Madrid", UtilDate.newDate(year,month,28));
 
@@ -54,12 +64,12 @@ public class HibernateDataAccess {
 		Question q6;
 
 		if (Locale.getDefault().equals(new Locale("es"))) {
-			q1=ev1.addQuestion("¿Quién ganará el partido?",1);
-			q2=ev1.addQuestion("¿Quién meterá el primer gol?",2);
-			q3=ev11.addQuestion("¿Quién ganará el partido?",1);
-			q4=ev11.addQuestion("¿Cuántos goles se marcarán?",2);
-			q5=ev17.addQuestion("¿Quién ganará el partido?",1);
-			q6=ev17.addQuestion("¿Habrá goles en la primera parte?",2);
+			q1=ev1.addQuestion("¿Quien ganara el partido?",1);
+			q2=ev1.addQuestion("¿Quien metera gol?",2);
+			q3=ev11.addQuestion("¿Quien ganara el partido?",1);
+			q4=ev11.addQuestion("¿Cuantos goles marcaran?",2);
+			q5=ev17.addQuestion("¿Quien ganara el partido?",1);
+			q6=ev17.addQuestion("¿Habra goles en la primera parte?",2);
 		}
 		else if (Locale.getDefault().equals(new Locale("en"))) {
 			q1=ev1.addQuestion("Who will win the match?",1);
